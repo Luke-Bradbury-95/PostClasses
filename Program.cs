0b373feca7614d91e1823483ab7d556c7829e255
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-04-03 19:22:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+
+Post post1 = new Post("Thanks for the teaching Sauv", true, "Luke B");
+Post post2 = new Post("omg youll never reach the top WATCHING pokemon, but its okay we dont need competitors on this mountain", true, "Sauv");
+Console.WriteLine(post1);
+Console.WriteLine(post2);
+Console.ReadLine();
+ImagePost imagePost = new ImagePost("Check my meatballs out", "Luke Bradbury", "https://image.com/meatballs",true);
+Console.WriteLine(imagePost.ToString());
+Console.ReadLine();
+VideoPost videoPost = new VideoPost("Video teaching sauv motivational speeches", "Luke Bradbury", true, "https://video.com/Motivation", 120);
+Console.WriteLine(videoPost.ToString());
+Console.ReadKey();
---
+++ VideoPost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class VideoPost:Post
+    {
+        public string VideoURL {  get; set; }
+        public float Length { get; set; }
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, bool isPublic, string videoURL, int Length)
+        {
+            this.ID = GetNextId();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+            this.VideoURL = videoURL;
+            this.Length = Length;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - {3}  by {4}", this.ID, this.Title, this.VideoURL, this.Length, this.SendByUsername);
+        }
+        public static void  Play()
+        {
+            Console.WriteLine("Video has now started playing");
+            
+        }
+
+    }
+}
